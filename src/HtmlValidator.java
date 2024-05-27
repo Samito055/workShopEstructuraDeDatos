29436e4792d278da7b5c786577b29124495b0717
Samito055 <samuelelteso@gmail.com>
2024-05-27 12:53:48 -0500

    WorkShop Terminado

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,16 +1,48 @@
+import javax.swing.text.html.HTML;
 import java.util.Queue;
 import java.util.Stack;
 
 
 public class HtmlValidator {
 
+
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
 
-		/* IMPLEMENT THIS METHOD! */
+		Stack<HtmlTag> stack = new Stack<>();
 
-		return null; // this line is here only so this code will compile if you don't modify it
+		while (!tags.isEmpty()) {
+
+			HtmlTag currentTag = tags.poll();
+
+
+			if (currentTag.isSelfClosing()) {
+				continue;
+
+			}else if (currentTag.isOpenTag()) {
+				stack.push(currentTag);
+			} else {
+				if (stack.isEmpty()) {
+					return null;
+				}
+
+				HtmlTag openTag = stack.peek();
+
+				if (currentTag.matches(openTag)) {
+					stack.pop();
+				} else {
+					return stack;
+				}
+
+				if (tags.isEmpty()) {
+					return stack;
+				}
+			}
+		}
+			return null; // this line is here only so this code will compile if you don't modify it
+
+		}
 	}
-	
 
-}
+
+
 
